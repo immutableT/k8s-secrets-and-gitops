74196d12998e7d8862141b89f73b70851d69988b
immutablet <alextc@google.com>
2020-02-11 16:39:37 -0800

    Add self-signed certs for kube-apiserver via kube-apiserver gen process.

--- scripts/kas-start.sh
+++ scripts/kas-start.sh
@@ -7,4 +7,4 @@ if sudo lsof -ti:${SECURE_PORT}; then
  sudo lsof -ti:${SECURE_PORT} | xargs kill -9
 fi
 
-../bin/kube-apiserver --secure-port=${SECURE_PORT} --etcd-servers=http://127.0.0.1:2379 --storage-backend=etcd3 --tls-cert-file=../certs/kube-apiserver.crt --tls-private-key-file=../certs/kube-apiserver.key --logtostderr=true
+../bin/kube-apiserver --secure-port=${SECURE_PORT} --etcd-servers=http://127.0.0.1:2379 --storage-backend=etcd3 --cert-dir=./apiserver.local.config/certificates  #--tls-cert-file=../certs/kube-apiserver.crt --tls-private-key-file=../certs/kube-apiserver.key --logtostderr=true

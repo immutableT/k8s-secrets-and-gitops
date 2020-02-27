21639260dfa8e76b37dccb51b603a50e4b1eda7e
immutablet <alextc@google.com>
2020-02-26 16:55:54 -0800

    Create SA and grant it IAM permissions on the key.

--- scripts/security_admin.sh
+++ scripts/security_admin.sh
@@ -5,6 +5,7 @@ PROJECT="alextc-gke-dev"
 LOCATION="us-central1"
 KEY_RING="kubecon-eu-demo-ring"
 KEY_NAME="kubecon-eu-key"
+SA_NAME="secrets-decrypter-sa"
 
 gcloud kms keys create "${KEY_NAME}" \
   --project "${PROJECT}" \
@@ -12,6 +13,20 @@ gcloud kms keys create "${KEY_NAME}" \
   --keyring "${KEY_RING}" \
   --purpose asymmetric-encryption \
   --default-algorithm rsa-decrypt-oaep-4096-sha256
+
+
+gcloud iam service-accounts create "${SA_NAME}" \
+    --description "SA used for decrypting secrets" \
+    --display-name "secrets-decrypter-sa" \
+    --project "${PROJECT}"
+
+
+gcloud kms keys add-iam-policy-binding "${KEY_NAME}" \
+  --location "${LOCATION}" \
+  --keyring "${KEY_RING}" \
+  --member "serviceAccount:${SA_NAME}@${PROJECT}.iam.gserviceaccount.com" \
+  --role roles/cloudkms.cryptoKeyDecrypter \
+  --project "${PROJECT}"
 
 gcloud kms keys versions  \
   get-public-key 1 \
